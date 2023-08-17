076b6e5871ef0b8faf0978572021538e22575969
maetin <maetin0324@gmail.com>
2023-08-17 12:39:13 +0900

    fuga

--- mod_test/mod_test.c
+++ mod_test/mod_test.c
@@ -13,8 +13,8 @@ int main()
 	int emoji_state;
 	int emoji_state_one = 1;
 
-	if ((fd0_A = open("/dev/emoji_device0", O_RDWR)) < 0) perror("open");
-	if ((fd0_B = open("/dev/emoji_device0", O_RDWR)) < 0) perror("open");
+	if ((fd0_A = open("/dev/emoji_device", O_RDWR)) < 0) perror("open");
+	if ((fd0_B = open("/dev/emoji_device", O_RDWR)) < 0) perror("open");
 
 	if (ioctl(fd0_A, EMOJI_DEVICE_GET_VALUES, &emoji_state) < 0) perror("ioctl_get");
 	printf("state0 = %d\n", emoji_state);
@@ -23,9 +23,9 @@ int main()
 	if (write(fd0_B, "0_B", 4) < 0) perror("write");
 
 	if (read(fd0_A, buff, 16) < 0) perror("read");
-	printf(":bow: > /dev/emoji_device0 ->%s\n", buff);
+	printf(":bow: > /dev/emoji_device ->%s\n", buff);
 	if (read(fd0_B, buff, 16) < 0) perror("read");
-	printf("0_B > /dev/emoji_device0 -> %s\n", buff);
+	printf("0_B > /dev/emoji_device -> %s\n", buff);
 
 	if (ioctl(fd0_A, EMOJI_DEVICE_SET_VALUES, &emoji_state_one) < 0) perror("ioctl_set");
 	if (ioctl(fd0_A, EMOJI_DEVICE_GET_VALUES, &emoji_state) < 0) perror("ioctl_get");
@@ -33,7 +33,7 @@ int main()
 	printf("state1 = %d\n", emoji_state);
 
 	if (read(fd0_A, buff, 16) < 0) perror("read");
-	printf(":bow: > /dev/emoji_device0 ->%s\n", buff);
+	printf(":bow: > /dev/emoji_device ->%s\n", buff);
 
 	if (ioctl(fd0_A, EMOJI_DEVICE_CLEAR_VALUES) < 0) perror("ioctl_clear");
 	if (ioctl(fd0_A, EMOJI_DEVICE_GET_VALUES, &emoji_state) < 0) perror("ioctl_get2");
